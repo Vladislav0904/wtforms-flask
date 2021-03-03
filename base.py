fbdfec4fe5956fd0a0d0ecf3d5a00eb31772b3c1
Vladislav <vladislav0904@gmail.com>
2021-03-03 16:53:48 +0500

    Classwork Problem 3

--- base.py
+++ base.py
@@ -11,11 +11,22 @@ def index(title):
 
 @app.route('/training/<prof>')
 def training(prof):
-    print({url_for('static', filename='img/spaceship_is.jpg')})
     if 'инженер' in prof.lower() or 'строитель' in prof.lower():
         return render_template('training.html', ing=True)
     else:
         return render_template('training.html')
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    specialities = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+                    'климатолог', 'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                    'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер', 'штурман',
+                    'пилот дронов']
+    if 'ol' in list or 'ul' in list:
+        return render_template('prof_list.html', list=list, specialities=specialities)
+    else:
+        return 'Передан неверный параметр. Используйте "ol" или "ul"!'
 
 
 if __name__ == '__main__':
